0470adb2cb3e08263f01638c90d202ad55c924d8
mystick minal <mystick14minal@gmail.com>
2024-09-23 10:27:00 +0545

    completed 4 labs of MAD

--- Login-class/lib/pages/dashboard.dart
+++ Login-class/lib/pages/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class dashboardPage extends StatefulWidget {
   const dashboardPage({super.key});
@@ -9,82 +10,99 @@ class dashboardPage extends StatefulWidget {
 }
 
 class _dashboardPageState extends State<dashboardPage> {
-
   horizontalscrollfunc(var size, var color) {
     return Container(
-      width: size.width / 1.5,
-      height: size.height / 4,
-      margin: EdgeInsets.only(left: 10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Stack(
-        children: [
-          Container(
-            width: size.width / 1.5,
-            height: size.height / 4,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Opacity(
-                opacity: 0.7, // Set opacity here (0.0 to 1.0)
-                child: Image.asset(
-                  "images/bg.jpeg",
-                  fit: BoxFit.cover,
-                ),
+        margin: EdgeInsets.only(left: 10),
+        width: size.width / 1.5,
+        height: size.height / 5,
+        decoration: BoxDecoration(
+            color: Color(color), borderRadius: BorderRadius.circular(20)),
+        child: Stack(
+            children:[
+              Container(
+                  height: size.height/5,
+                  width: size.width/1.5,
+                  child:ClipRRect(borderRadius:BorderRadius.circular(20),child: Image.asset("images/bg.jpeg",fit: BoxFit.cover,),)
               ),
-            ),
-          ),
-          Positioned(
-            top: 10,
-            left: 10,
-            child: Container(
-              width: size.width / 2,
-              decoration: BoxDecoration(
-                color: Colors.black12,
+              Positioned(
+                  bottom:40,
+                  left: 15,
+                  child:  Container(
+                      width: size.width/2,
+                      child:  Text("The greatest of all time - kyrie",
+                        style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+                        maxLines: 2,))
               ),
-              child: Text(
-                "Charitra is handsome",
-                style: TextStyle(color: Colors.white),
+              Positioned(
+                  bottom:10,
+                  left: 15,
+                  child:  Container(
+                      width: size.width/2,
+                      color: Colors.black12,
+                      child:  Text("Sep 20, 2024",
+                        style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+                        maxLines: 1,overflow: TextOverflow.ellipsis,))
               ),
-            ),
-          ),
-          Positioned(
-            bottom: 30,
-            left: 10,
-            child: Text(
-              "The b",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+              Positioned(
+                  right: 5,
+                  bottom: 5,
+                  child:
+                  Icon(Icons.play_circle, size: 30,color: Colors.white,))
+            ]
+        )
+    );
+  }
+  verticalscrollfunc(var size) {
+    return Container(
+        margin: EdgeInsets.only(bottom: 10, top: 10, left: 10),
+        width: size.width / 0.5,
+        height: size.height / 7,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+        child: Stack(
+            children:[
+              Container(
+                  height: size.height/7,
+                  width: size.width/2.5,
+                  child:ClipRRect(borderRadius:BorderRadius.circular(20),child: Image.asset("images/bg.jpeg",fit: BoxFit.cover,),)
               ),
-              maxLines: 3,
-            ),
-          ),
-          Positioned(
-            bottom: 10,
-            left: 10,
-            child: Text(
-              "The greatest of all time",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 10,
-                fontWeight: FontWeight.bold,
+              Positioned(
+                  top:5,
+                  right: 15,
+                  child:  Container(
+                      width: size.width/2,
+                      child:  Text("charitra is female",
+                        style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+                        maxLines: 2,))
               ),
-              maxLines: 3,
-            ),
-          ),
-          Positioned(
-            bottom: 5,
-            right: 5,
-            child: Icon(
-              Icons.star,
-              color: Colors.white,
-              size: 30,
-            ),
-          ),
-        ],
-      ),
+              Positioned(
+                  bottom:10,
+                  right: 5,
+                  child:  Container(
+                      width: size.width/3.5,
+                      child:  Text("Sep 20, 2024",
+                        style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold),
+                        maxLines: 1,overflow: TextOverflow.ellipsis,))
+              ),
+              Positioned(
+                  bottom:10,
+                  left: 175,
+                  child:  Container(
+                      width: size.width/4.5,
+                      decoration: BoxDecoration(
+                        color: Colors.red, // Move color here
+                        borderRadius: BorderRadius.circular(5), // Set border radius
+                      ),
+                      child:  const Text("xyz.com",
+                        style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold), textAlign: TextAlign.center,
+                        maxLines: 1,overflow: TextOverflow.ellipsis,))
+              ),
+            const  Positioned(
+                  top: 40,
+                  left: 60,
+                  child:
+                  Icon(Icons.play_circle, size: 50,color: Colors.white,))
+            ]
+        )
     );
   }
 
@@ -97,30 +115,47 @@ class _dashboardPageState extends State<dashboardPage> {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  horizontalscrollfunc(size, 0xfffff00f), // Pass color argument
-                  SizedBox(width: 10),
-                  horizontalscrollfunc(size, 0xfffff00fa), // Pass color argument
-                  SizedBox(width: 10),
-                  horizontalscrollfunc(size, 0xfffff1115), // Pass color argument
-                  SizedBox(width: 10),
-                  horizontalscrollfunc(size, 0xfffff4445), // Pass color argument
-                ],
-              )
-          ),
-          SizedBox(height: 50),
+          // SingleChildScrollView(
+          //     scrollDirection: Axis.horizontal,
+          //     child: Row(
+          //       children: [
+          //         horizontalscrollfunc(size, 0xfffff00f),
+          //         const SizedBox(
+          //           width: 10,
+          //         ),
+          //         horizontalscrollfunc(size, 0xfffff00fa),
+          //         const SizedBox(
+          //           width: 10,
+          //         ),
+          //         horizontalscrollfunc(size, 0xfffff1115),
+          //         const SizedBox(
+          //           width: 10,
+          //         ),
+          //         horizontalscrollfunc(size, 0xfffff4445),
+          //       ],
+          //     )),
           Container(
-            height: size.height / 2,
-            child: ListView.builder(
-              itemCount: 20,
-              itemBuilder: (context, index) {
-                return horizontalscrollfunc(size, Colors.blueAccent); // Adding color
-              },
+            height: size.height/5,
+            width: size.width/1,
+            child: ListView.builder(itemCount: 20,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index){
+                  return horizontalscrollfunc(size, 0xfffff1115);
+                }
             ),
           ),
+          SizedBox(height: 10,),
+          Container(
+            height: size.height/1.45,
+            width: size.width/0.5,
+            child: ListView.builder(itemCount: 20,
+                scrollDirection: Axis.vertical,
+                itemBuilder: (context, index){
+                  return verticalscrollfunc(size);
+                }
+            ),
+          )
+
         ],
       ),
     );
